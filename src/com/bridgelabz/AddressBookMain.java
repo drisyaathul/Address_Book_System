481532c7fc7e96f74150a7789b47b772f35a196d
drisyaathul <drisyak809@gmail.com>
2023-03-11 00:45:21 +0800

    Added: Editing Existing Contact Person

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -30,14 +30,85 @@ public class AddressBookMain {
         newContacts.seteMail(scanner.nextLine());
         contactDetails.add(newContacts);
     }
+    public void editDetails(String searchFirstName) {
+        for (AddressBook contact : contactDetails) {
+            String name = contact.getFirstName();
+            if (name.equals(searchFirstName)) {
+
+                System.out.println("The Details of the Person:- ");
+                System.out.println("1. First Name \n2. Last Name \n3. Address \n4. City \n5. State \n6. Zip Code \n7. Contact No \n8. Email");
+                System.out.println("Enter the value to Edit : ");
+                int num = scanner.nextInt();
+                switch (num) {
+                    case 1:
+                        System.out.println("Enter the first name You want to edit");
+                        String updatedFirstName = scanner.next();
+                        contact.setFirstName(updatedFirstName);
+                        displayDetails();
+                        break;
+                    case 2:
+                        System.out.println("Enter the Second Name You want to edit");
+                        String updatedSecondName = scanner.next();
+                        contact.setSecondName(updatedSecondName);
+                        displayDetails();
+                        break;
+                    case 3:
+                        System.out.println("Enter the Address You want to edit");
+                        String updatedAddress = scanner.next();
+                        contact.setAddress(updatedAddress);
+                        displayDetails();
+                        break;
+                    case 4:
+                        System.out.println("Enter the City You want to edit");
+                        String updatedCity = scanner.next();
+                        contact.setCity(updatedCity);
+                        displayDetails();
+                        break;
+                    case 5:
+                        System.out.println("Enter the State You want to edit");
+                        String updatedState = scanner.next();
+                        contact.setState(updatedState);
+                        displayDetails();
+                        break;
+                    case 6:
+                        System.out.println("Enter the Zip code You want to edit");
+                        String updatedZipCode = scanner.next();
+                        contact.setZip(updatedZipCode);
+                        displayDetails();
+                        break;
+                    case 7:
+                        System.out.println("Enter the Contact numberYou want to edit");
+                        String updatedContact = scanner.next();
+                        contact.setPhoneNumber(updatedContact);
+                        displayDetails();
+                        break;
+                    case 8:
+                        System.out.println("Enter the email You want to edit");
+                        String updatedEmail = scanner.next();
+                        contact.seteMail(updatedEmail);
+                        displayDetails();
+                        break;
+                    default:
+                        System.out.println("Invalid number!");
+                }
+            }
+            else{
+                System.out.println("No record found!");
+            }
+        }
+    }
     public void displayDetails(){
         System.out.println(contactDetails);
     }
     public static void main(String[] args) {
         System.out.println("**** Welcome to ADDRESS BOOK PROGRAM ****");
-
+        Scanner scanner = new Scanner(System.in);
         AddressBookMain person = new AddressBookMain();
         person.AddNewContact();
         person.displayDetails();
+
+        System.out.println("Enter the First Name for which you want to Edit the Details: ");
+        String nameFirst = scanner.next();
+        person.editDetails(nameFirst);
     }
 }
