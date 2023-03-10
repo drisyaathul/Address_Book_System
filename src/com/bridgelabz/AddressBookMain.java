de0cad6862012d492a6ff5cb9f092d11b1ebc1b1
drisyaathul <drisyak809@gmail.com>
2023-03-10 19:50:17 +0800

    Added: Creating Contacts in Address Book

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,7 +1,26 @@
 package com.bridgelabz;
 
 public class AddressBookMain {
+
     public static void main(String[] args) {
         System.out.println("**** Welcome to ADDRESS BOOK PROGRAM ****");
+
+        AddressBook contacts = new AddressBook();
+        contacts.setFirstName("Drisya");
+        System.out.println(contacts.getFirstName());
+        contacts.setSecondName("Athul");
+        System.out.println(contacts.getSecondName());
+        contacts.setAddress("Sreyas House");
+        System.out.println(contacts.getAddress());
+        contacts.setCity("Calicut");
+        System.out.println(contacts.getCity());
+        contacts.setState("Kerala");
+        System.out.println(contacts.getState());
+        contacts.setZip("673501");
+        System.out.println(contacts.getZip());
+        contacts.setPhoneNumber("0112345250");
+        System.out.println(contacts.getPhoneNumber());
+        contacts.seteMail("drisyak809@gmail.com");
+        System.out.println(contacts.geteMail());
     }
 }
